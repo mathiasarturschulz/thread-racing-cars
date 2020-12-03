991cdd0be106d58b91a2d2b3119d59ddbcd9b4e0
Mathias Schulz <MathiasSchulz34@gmail.com>
2020-12-02 21:46:01 -0300

    [UPD] Atualizado o metodo de pit stop

---
+++ app/src/controller/ControllerPitStop.java
@@ -0,0 +1,36 @@
+package src.controller;
+
+import src.model.ModelCarro;
+
+/**
+ * 
+ * Classe que controla as operações com o pit stop
+ * 
+ * @author Mathias Artur Schulz
+ * @since 02/12/2020
+ */
+public class ControllerPitStop {
+
+	private final static float QTD_PARA_REABASTECER = 50;
+
+	/**
+	 * Método responsável por realizar o pit stop do carro atual
+	 * O pit stop só pode ser realizado um carro por vez
+	 *  
+	 * @param modelCarro
+	 * @param combustivelCarro
+	 * @return
+	 * @throws InterruptedException
+	 */
+	public static synchronized float pitStop(ModelCarro modelCarro, float combustivelCarro) throws InterruptedException {
+		System.out.println("=> " + modelCarro.getPiloto() + " (" + modelCarro.getNome() + ") ENTROU no pit stop! ");
+
+		// realizar o reabastecimento do carro
+		combustivelCarro += QTD_PARA_REABASTECER;
+		// realiza uma pausa na thread do carro para simular o tanque sendo enchido
+		Thread.sleep(300);
+
+		System.out.println("=> " + modelCarro.getPiloto() + " (" + modelCarro.getNome() + ") SAIU do pit stop! ");
+		return combustivelCarro;
+	}
+}
